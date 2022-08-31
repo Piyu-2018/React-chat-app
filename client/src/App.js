667c75bfabc91f947d9deefbe6018fc3774d2a92
Piyumi Rathnayaka <2019cs138@stu.ucsc.lk>
2022-08-31 18:19:30 +0530

    This commit for updating chat app

--- client/src/App.js
+++ client/src/App.js
@@ -21,22 +21,22 @@ function App() {
     <div className="App">
       {!showChat ? (
         <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
+          <h3>WeChat</h3>
           <input
             type="text"
-            placeholder="John..."
+            placeholder="Piyumi...."
             onChange={(event) => {
               setUsername(event.target.value);
             }}
           />
           <input
             type="text"
-            placeholder="Room ID..."
+            placeholder="Room ID No..."
             onChange={(event) => {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>
+          <button onClick={joinRoom}>Choose A Room</button>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
